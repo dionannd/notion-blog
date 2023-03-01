86f48fcd5548ea0a038837599a9af18eb995c933
dionannd <dionananda77@gmail.com>
2023-03-01 15:41:03 +0700

    fix(meta): change meta & default fallback image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import PublicNotionCopier from './src/integrations/public-notion-copier';
 import react from '@astrojs/react';
 import image from '@astrojs/image';
 import tailwind from '@astrojs/tailwind';
+import addClasses from 'rehype-add-classes';
 
 const getSite = function () {
   if (!process.env.CF_PAGES) {
@@ -36,4 +37,23 @@ export default defineConfig({
     image(),
     tailwind(),
   ],
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold font-mplus',
+          h2: 'text-2xl font-bold font-mplus',
+          h3: 'text-xl font-bold font-mplus',
+          h4: 'text-lg font-bold font-mplus',
+          h5: 'font-bold font-mplus',
+          h6: 'font-bold font-mplus',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-orange-500 decoration-orange-500',
+        },
+      ],
+    ],
+  },
 });
